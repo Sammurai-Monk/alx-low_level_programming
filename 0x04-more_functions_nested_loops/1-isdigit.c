a885168cb1bef27d5f4b560d235e5d6ce2c6681c
Sammurai <asoyosammy@gmail.com>
2023-02-24 13:27:40 -0800

    1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit -Check if character is dig
+ * @x: The num
+ * Return: 1 for char 0 anyelse
+ */
+
+int _isdigit(int x)
+{
+	if (x >= 48 && <= 57)
+	{
+		return (1);
+	}
+	return (0);
+}
