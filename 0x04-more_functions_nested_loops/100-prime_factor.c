edaf2c189dd01a74425651af2b5ae42776c4e2b4
Sammurai <asoyosammy@gmail.com>
2023-02-24 14:45:02 -0800

    100

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main -Check if character is dig
+ * Return: 1 for char 0 anyelse
+ */
+
+int main(void)
+{
+	long x, maxf;
+	long number = 612852475143;
+	double square = sqrt(number);
+
+	for (x = 1; x <= square; x++)
+	{
+		if (number % x == 0)
+		{
+			maxf = number / x;
+		}
+	}
+	printf("%ld\n", maxf);
+	return (0);
+}
