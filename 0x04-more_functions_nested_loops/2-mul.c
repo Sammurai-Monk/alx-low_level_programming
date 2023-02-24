8f5bada727f80616dff1c63c2aa63a4f2ab714c1
Sammurai <asoyosammy@gmail.com>
2023-02-24 13:32:43 -0800

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul -Check if character is dig
+ * @x: The num
+ * @y: The secon num
+ * Return: 1 for char 0 anyelse
+ */
+
+int mul(int x, int y)
+{
+	return (x * y);
+}
