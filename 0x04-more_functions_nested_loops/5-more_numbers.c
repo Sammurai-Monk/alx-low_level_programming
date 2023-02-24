337f5eb206c6bc4aca5877316606d2b944c3d8d6
Sammurai <asoyosammy@gmail.com>
2023-02-24 13:49:01 -0800

    5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - Check if character is dig
+ * Return: 1 for char 0 anyelse
+ */
+
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			if (y > 9)
+			{
+				_putchar((y / 10) + '0');
+			}
+			_putchar((y % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
