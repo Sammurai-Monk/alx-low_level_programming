13195f18ce99478fd01a2da0554c59b05961df40
Sammurai <asoyosammy@gmail.com>
2023-02-24 13:20:57 -0800

    0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - Check if
+ * @x: The no
+ * Return: 1 upper 0 else
+ */
+
+int _isupper(int x)
+{
+	if (x >= 65 && x <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
