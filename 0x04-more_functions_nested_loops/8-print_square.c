a3eb0e0a9296fbb2b5fd523b63622b1f8dac3ae3
Sammurai <asoyosammy@gmail.com>
2023-02-24 14:05:59 -0800

    8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_square -Check if character is dig
+ * @size: The num
+ * Return: 1 for char 0 anyelse
+ */
+
+void print_square(int size)
+{
+	int x, y;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (y = 0; y < size; y++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
