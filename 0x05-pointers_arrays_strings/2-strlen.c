71e758b222c169de5bafe8570e089ef34a3f51a1
Sammurai <asoyosammy@gmail.com>
2023-02-27 06:07:52 -0800

    2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * _strlen -Check if character is dig
+ * @str: The num
+ * Return: 1 for char 0 anyelse
+ */
+
+size_t _strlen(const char *str)
+{
+	size_t length = 0;
+
+	while (*str++)
+		length++;
+	return (lenght);
+}
